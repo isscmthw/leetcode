cb3949e3d77d1fdae52976e80b972ed5b18498c3
Issac Mathew <157203561+isscmthw@users.noreply.github.com>
2026-03-31 11:56:07 +0530

    [49] [Time Beats: 5.34%] [Memory Beats: 49.62%] - LeetPush

--- Group-Anagrams.py
+++ Group-Anagrams.py
@@ -1,12 +1,18 @@
 1class Solution:
 2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-3        d={}
-4        l=[]
-5        for word in strs:
-6            key="".join(sorted(word))
-7            if key not in d:
-8                d[key]=[word]
-9            else:
-10                d[key].append(word)
-11        return list(d.values())
-12
+3        def getfreqstring(element):
+4            freqlist=[0]*26
+5            for letter in element:
+6                freqlist[ord(letter)-ord('a')]+=1
+7            freqstring=[]
+8            char='a'
+9            for i in freqlist:
+10                freqstring.append(char)
+11                freqstring.append(str(i))
+12                char=chr(ord(char)+1)
+13            return "".join(freqstring)
+14        d={}
+15        for word in strs:
+16            key=getfreqstring(word)
+17            d.setdefault(key,[]).append(word)
+18        return list(d.values())
